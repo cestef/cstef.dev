fda14c82d87760e404fdacca61831782cea35491
cstef <53212129+cstefFlexin@users.noreply.github.com>
2021-10-14 18:54:10 +0200

    Add project

--- src/routes/Projects/index.tsx
+++ src/routes/Projects/index.tsx
@@ -4,10 +4,28 @@ import { Container, Grid } from "@mui/material";
 import styles from "./styles";
 const PROJECTS: ProjectProps[] = [
     {
+        title: "Spotify Card",
+        description: "Generate Spotify embeds for any song !",
+        github: "https://github.com/cstefFlexin/spotify-card",
+        image: "/images/spotify-card.png",
+    },
+    {
+        title: "Kotlin Formatter",
+        description: "Format your Kotlin code easily in VScode",
+        link: "https://marketplace.visualstudio.com/items?itemName=cstef.kotlin-formatter",
+        github: "https://github.com/cstefFlexin/kotlin-formatter",
+        image: "https://avatao.com/file/2020/08/1_fnbqF0xNVwINs_RkygkX1g.png",
+    },
+    {
         title: "Vintools",
         description: "Find your favorite clothes from Vinted® easily !",
         image: "https://vintools.cstef.dev/images/logo_vintools.png",
         link: "https://vintools.cstef.dev",
+    },
+    {
+        title: "cstef's API",
+        description: "My personal RESTful API serving endpoints to test out my packages !",
+        link: "https://api.cstef.dev",
     },
     {
         title: "React JSON Translator",
@@ -23,13 +41,7 @@ const PROJECTS: ProjectProps[] = [
         github: "https://github.com/cstefFlexin/web-player",
         image: "https://i.pinimg.com/originals/73/05/37/7305374c6ae8e92347615e659a81ab36.png",
     },
-    {
-        title: "Kotlin Formatter",
-        description: "Format your Kotlin code easily in VScode",
-        link: "https://marketplace.visualstudio.com/items?itemName=cstef.kotlin-formatter",
-        github: "https://github.com/cstefFlexin/kotlin-formatter",
-        image: "https://avatao.com/file/2020/08/1_fnbqF0xNVwINs_RkygkX1g.png",
-    },
+
     {
         title: "Auto-Updater",
         description:
@@ -43,12 +55,6 @@ const PROJECTS: ProjectProps[] = [
         github: "https://github.com/cstefFlexin/cstef.dev",
         image: "/android-chrome-512x512.png",
         link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-    },
-    {
-        title: "Spotify Card",
-        description: "Generate Spotify embeds for any song !",
-        github: "https://github.com/cstefFlexin/spotify-card",
-        image: "/images/spotify-card.png",
     },
 ];
 
