b7be333ddde2c7ccefc3541e9d57346be9bf2eed
cstef <colin@petit-suisse.fr>
2021-10-25 23:25:09 +0200

    testing again

--- src/routes/Contact/index.tsx
+++ src/routes/Contact/index.tsx
@@ -5,7 +5,7 @@ export const Contact = () => {
     return (
         <Container sx={styles.root}>
             <Typography variant="h3" color="text.primary">
-                Contact me
+                Contact me !
             </Typography>
             <Typography variant="h6" color="text.secondary">
                 Want my email ? Let's play a game then ;) <br />
