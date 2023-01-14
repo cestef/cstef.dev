9b0c07e278ac1e4e9a824ed07f4d7fe3e4d2f3b1
cstef <colin@petit-suisse.fr>
2023-01-14 20:05:13 +0100

    Twemoji CDN has shutdown ?

--- src/components/Twemoji/index.tsx
+++ src/components/Twemoji/index.tsx
@@ -1,12 +1,16 @@
-import React, { memo } from "react";
-import twemoji from "twemoji";
 import "./Twemoji.css";
+
+import React, { memo } from "react";
+
+import twemoji from "twemoji";
+
 const Twemoji = ({ emoji }: { emoji: string }) => (
     <span
         dangerouslySetInnerHTML={{
             __html: twemoji.parse(emoji, {
                 folder: "svg",
                 ext: ".svg",
+                base: "https://cdnjs.cloudflare.com/ajax/libs/twemoji/14.0.2/",
             }),
         }}
     />
